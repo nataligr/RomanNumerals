df167a77e62e86382d70efa62790da46f34359fd
natali <natalimo@post.jce.ac.il>
2012-12-15 23:28:16 +0200

    RED

--- src/RomanNumeralUnitTests.java
+++ src/RomanNumeralUnitTests.java
@@ -12,8 +12,8 @@ public class RomanNumeralUnitTests {
 	//	assertEquals("I", number.toRoman(1));//GREEN I
 		//assertEquals("CCCXXXII", number.toRoman(333));//RED CCCXXXIII
 		//assertEquals("CCCXXXIII", number.toRoman(333));//GREEN CCCXXXIII
-		assertEquals("MI", number.toRoman(1001));//GREEN MI
-		//assertEquals("DCL", number.toRoman(555));//RED DLV
+	//	assertEquals("MI", number.toRoman(1001));//GREEN MI
+		assertEquals("DCL", number.toRoman(555));//RED DLV
 	//	assertEquals("CCCLXIX", number.toRoman(369));//GREEN CCCLXIX
 	//	assertEquals("II", number.toRoman(123));//RED CXXIII
 		//assertEquals("MMMC", number.toRoman(4000));//RED MMMM
